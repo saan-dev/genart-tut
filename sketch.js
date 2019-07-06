0c3bea8983c3085480479b2b677bb192804e0522
unknown <saanrashid@hotmail.com>
2019-07-06 22:56:37 +0200

    Radius and Organic Randomness

--- sketch.js
+++ sketch.js
@@ -14,7 +14,10 @@ const sketch = () => {
             for(let y = 0; y < count; y++){
                 const u = count <= 1 ?  0.5 : x / (count - 1) ;
                 const v = count <= 1 ?  0.5 : y / (count - 1) ;
-                points.push([ u, v ]);
+                points.push({
+                        radius: Math.abs(0.01 + random.gaussian() * 0.01),
+                        position: [ u, v ]
+                });
             }
         }
         return points;
@@ -27,15 +30,23 @@ const sketch = () => {
         context.fillStyle = 'white';
         context.fillRect(0,0,width,height);
 
-        points.forEach(([u, v]) => {
+        points.forEach(data => {
+            const {
+                position, 
+                radius
+            } = data;
+
+            const [u, v] = position;
+
             const x = lerp(margin, width - margin, u)
             const y = lerp(margin, height - margin, v)
             
             context.beginPath();
-            context.arc(x,y,10,0,Math.PI * 2,false);
+            context.arc(x,y,radius * width,0,Math.PI * 2,false);
             context.strokeStyle = 'black';
             context.lineWidth = 10;
-            context.stroke();
+            context.fillStyle = 'red'
+            context.fill();
         });
     };
 };
